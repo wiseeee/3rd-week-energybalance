995da192417cdbbb6a2d7ed7aaf651673f33268a
gilmujjang <alsanrlf@naver.com>
2022-02-07 17:07:33 +0900

    chore: cra init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'react-app',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+
+  rules: {
+    'import/no-cycle': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-alert': 'off',
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts', '.js'] }],
+    'no-console': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/prop-types': 'off',
+    'react/tsx-props-no-spreading': 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const a: string = 'hello world';
+
+function App() {
+  return <div className="App">{a}</div>;
+}
+
+export default App;
