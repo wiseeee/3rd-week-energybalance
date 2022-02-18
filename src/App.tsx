a348668e8bee9a2817cd8100379bf5f208e9b78e
gilmujjang <alsanrlf@naver.com>
2022-02-18 12:05:35 +0900

    feat: 검색기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import Search from './components/Search/index';
-import Loading from './components/Loading/index';
 import SelectBox from './components/SelectBox/index';
 import View from './components/View/index';
 import axios from 'axios';
@@ -11,7 +11,6 @@ type Items = {
   브랜드: string | null;
 };
 const App: React.FC = () => {
-  const [views, setViews] = useState<Items[]>([]);
   const [items, setItems] = useState<Items[]>([]);
   const [brands, setBrands] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
@@ -20,40 +19,9 @@ const App: React.FC = () => {
   const [searchHistory, setSearchHistory] = useState<string[]>([]);
   const [selected, setSelected] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setError(null);
-        setItems([]);
-        setLoading(true);
-        const response = await axios.get(`${MOCK_URL}`);
-        const brandsResponse = await axios.get(`${MOCK_URL}/nutrients`);
-        const { data } = response;
-        setItems(data);
-        setViews(data);
-        setBrands(brandsResponse.data.brands);
-        setLoading(false);
-      } catch (err: unknown) {
-        if (err instanceof Error) {
-          return {
-            message: `Things exploded (${err.message})`,
-          };
-        }
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const target = e.target.value;
     setInput(target);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    changeSearchHistory();
-    setInput('');
   };
 
   const deleteSearchHistory = (
@@ -78,15 +46,60 @@ const App: React.FC = () => {
   const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const target = e.target.value;
     setSelected(target);
-    if (target) {
-      const filteredItems = items.filter((item) => item.브랜드 === target);
-      setViews(filteredItems);
-    } else {
-      setViews(items);
-    }
   };
 
-  if (loading) return <Loading />;
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    changeSearchHistory();
+    setInput('');
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setError(null);
+        setLoading(true);
+        const brandsResponse = await axios.get(`${MOCK_URL}`);
+        setBrands(brandsResponse.data.brands);
+        setLoading(false);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          return {
+            message: `Things exploded (${err.message})`,
+          };
+        }
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setError(null);
+        setItems([]);
+        setLoading(true);
+        const response = await axios.get(
+          `${MOCK_URL}/nutrients?keyword=${input}`,
+        );
+        const { data } = response;
+        setItems(data);
+
+        setLoading(false);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          return {
+            message: `Things exploded (${err.message})`,
+          };
+        }
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [input]);
+  console.log(items);
+
   if (error) return <div>에러가 발생했습니다</div>;
   if (!items) return null;
 
@@ -104,7 +117,7 @@ const App: React.FC = () => {
         handleSelect={handleSelect}
         brands={brands}
       />
-      <View />
+      <View loading={loading} />
     </>
   );
 };
--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Loading from '../Loading';
 
-const View: React.FC = () => {
-  return <div>View</div>;
+type Props = {
+  loading: boolean;
 };
 
+const View: React.FC<Props> = ({ loading }) => {
+  return <>{loading ? <Loading></Loading> : <div>asdf</div>}</>;
+};
 export default View;
