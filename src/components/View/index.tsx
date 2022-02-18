94d7d61213cd375c6dcba0dc9d6df184a3c04a4e
gilmujjang <alsanrlf@naver.com>
2022-02-18 19:42:10 +0900

    fix: need unique key error 처리

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { Items } from '../../App';
 import * as S from './styled';
@@ -8,8 +9,8 @@ type Props = {
 const View: React.FC<Props> = ({ view }) => {
   return (
     <S.ItemList>
-      {view.map((item) => (
-        <a href="#">
+      {view.map((item, index) => (
+        <a href="#" key={index}>
           <S.ItemWrap>
             <S.ItemsBrand>{item.브랜드}</S.ItemsBrand>
             <S.ItemsName>{item.제품명}</S.ItemsName>
